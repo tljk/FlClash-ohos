ec473672dbe53bfdc2195b771db19f0be0665619
tljk <tanglinchuanz@163.com>
2026-07-26 12:30:44 +0800

    update ohos

--- ohos/core/hvigorfile.ts
+++ ohos/core/hvigorfile.ts
@@ -1,6 +1,7 @@
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { setupPlugin } from '../../plugins/setup/buildkit/hvigor/setup'
 
 export default {
   system: harTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [setupPlugin()]       /* Custom plugin to extend the functionality of Hvigor. */
 }
